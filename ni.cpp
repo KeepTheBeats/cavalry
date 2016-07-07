1fca8fc1f2d62e98ec8316d8f964da1f64331e74
KeepTheBeats <447263@qq.com>
2016-07-07 08:37:45 +0800

    7月7日修改ni.cpp

--- ni.cpp
+++ ni.cpp
@@ -1,1 +1,13 @@
-test
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"ni"<<endl;
+    for(int i=0;i<10;i++){
+    	printf("%d\n",i);
+    }
+    return 0;
+}
